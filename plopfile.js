271ce5b89c4e7f00b28e7b07c6cc8945baa280ba
Otavio Augusto da Costa Pimentel <OtavioAugusto.daCostaPimentel-ext@ab-inbev.com>
2022-03-31 14:54:09 -0400

    Init

---
+++ plopfile.js
@@ -0,0 +1,31 @@
+export default function (plop) {
+  // create your generators here
+  plop.setGenerator("generic-component", {
+    description: "This is an example to automate react components generator",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Generic component name, please? (Pascal Case)",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/components/{{name}}/{{name}}.tsx",
+        templateFile: "plop-templates/GenericComponent.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{name}}/{{name}}.styles.ts",
+        templateFile: "plop-templates/GenericComponentStyle.ts.hbs",
+      },
+    ], // array of actions
+  });
+
+  plop.setHelper("dash-case", (text) => {
+    return text.replace(/[A-Z]/g, (letter, index) => {
+      return index === 0 ? letter.toLowerCase() : `-${letter.toLowerCase()}`;
+    });
+  });
+}
---
+++ src/components/NewComp/NewComp.styles.ts
@@ -0,0 +1,9 @@
+import { createStyles, makeStyles } from '@material-ui/core';
+
+export default makeStyles(({ palette }) =>
+  createStyles({
+    newCompContainer: {
+        display: 'flex'
+    }
+  }),
+);
